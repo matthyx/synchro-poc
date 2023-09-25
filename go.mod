bb6e767ce63839a5d0125ecfa63abcba54dacbc1
Matthias Bertschy <matthias.bertschy@gmail.com>
2023-09-25 08:52:16 +0200

    watch for deployments and generate patches
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,64 @@ module github.com/matthyx/synchro-poc
 
 go 1.20
 
-require github.com/wI2L/jsondiff v0.4.0 // indirect
+require (
+	github.com/kubescape/go-logger v0.0.21
+	github.com/wI2L/jsondiff v0.4.0
+	k8s.io/apimachinery v0.28.2
+	k8s.io/client-go v0.28.2
+)
+
+require (
+	github.com/briandowns/spinner v1.23.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.0 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/uptrace/opentelemetry-go-extra/otelutil v0.2.2 // indirect
+	github.com/uptrace/opentelemetry-go-extra/otelzap v0.2.2 // indirect
+	github.com/uptrace/uptrace-go v1.18.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/runtime v0.44.0 // indirect
+	go.opentelemetry.io/otel v1.18.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric v0.41.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v0.41.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.18.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.18.0 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.18.0 // indirect
+	go.opentelemetry.io/otel/metric v1.18.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.18.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v0.41.0 // indirect
+	go.opentelemetry.io/otel/trace v1.18.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/oauth2 v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230913181813-007df8e322eb // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230913181813-007df8e322eb // indirect
+	google.golang.org/grpc v1.58.1 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/klog/v2 v2.100.1 // indirect
+	k8s.io/utils v0.0.0-20230406110748-d93618cff8a2 // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,106 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"context"
+	"errors"
+	"flag"
+	"path/filepath"
 
+	"github.com/kubescape/go-logger"
+	"github.com/kubescape/go-logger/helpers"
 	"github.com/wI2L/jsondiff"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/watch"
+
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
+func getConfig() (*rest.Config, error) {
+	// try in-cluster config first
+	config, err := rest.InClusterConfig()
+	if err == nil {
+		return config, nil
+	}
+	// fallback to kubeconfig
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+	config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err == nil {
+		return config, nil
+	}
+	// nothing works
+	return nil, errors.New("unable to find config")
+}
+
+func newClient() (dynamic.Interface, error) {
+	config, err := getConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	dynClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return dynClient, nil
+}
+
 func main() {
-	vuln1, _ := os.ReadFile("test-data/nginx-1.json")
-	vuln2, _ := os.ReadFile("test-data/nginx-2.json")
-	patch, _ := jsondiff.CompareJSON(vuln1, vuln2)
-	fmt.Println(patch)
+	client, err := newClient()
+	if err != nil {
+		logger.L().Fatal("unable to create client", helpers.Error(err))
+	}
+	deploymentRes := schema.GroupVersionResource{Group: "apps", Version: "v1", Resource: "deployments"}
+	ctx := context.Background()
+	deployments := map[string]unstructured.Unstructured{}
+	watcher, err := client.Resource(deploymentRes).Namespace("default").Watch(ctx, metav1.ListOptions{})
+	if err != nil {
+		logger.L().Fatal("unable to watch for deployments", helpers.Error(err))
+	}
+	for {
+		event, chanActive := <-watcher.ResultChan()
+		if !chanActive {
+			watcher.Stop()
+			break
+		}
+		if event.Type == watch.Error {
+			logger.L().Error("watch event failed", helpers.Interface("event", event))
+			watcher.Stop()
+			break
+		}
+		d, ok := event.Object.(*unstructured.Unstructured)
+		if !ok {
+			continue
+		}
+		switch event.Type {
+		case watch.Added:
+			logger.L().Info("added deployment", helpers.String("name", d.GetName()))
+			deployments[d.GetName()] = *d
+		case watch.Modified:
+			logger.L().Info("modified deployment", helpers.String("name", d.GetName()))
+			if u, ok := deployments[d.GetName()]; ok {
+				source, _ := u.MarshalJSON()
+				target, _ := d.MarshalJSON()
+				patch, err := jsondiff.CompareJSON(source, target)
+				if err != nil {
+					logger.L().Error("cannot create patch", helpers.Error(err))
+				}
+				logger.L().Info("patch", helpers.String("name", patch.String()))
+			}
+		case watch.Deleted:
+			logger.L().Info("deleted deployment", helpers.String("name", d.GetName()))
+			delete(deployments, d.GetName())
+		}
+	}
 }
